b97f95127e48495f117299c8f6ea35f7a9e8efc3
Breno Salvador de Freitas <brenosalvadordefreitas@gmail.com>
2021-07-31 17:56:46 -0300

    Create SignUp page

---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import Form from "../components/Form";
+import GlobalStyle from "../globalStyles";
+
+export const SignUp: FC = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <Form />
+    </>
+  );
+}
